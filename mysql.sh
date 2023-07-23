571c6c4d8e1f6ef6ba532b2a046a371e90cd75e7
venkat <venkatapalivela.sap@gmail.com>
2023-07-23 07:13:44 +0530

    Roboshop scripts

---
+++ mysql.sh
@@ -0,0 +1,10 @@
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+yum module disable mysql -y
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl restart mysqld
+
+mysql_secure_installation --set-root-pass RoboShop@1
+
+

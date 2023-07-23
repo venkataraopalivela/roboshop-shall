07a4a6307005478e150e1482b093f6ae98618c1e
venkat <venkatapalivela.sap@gmail.com>
2023-07-23 07:32:22 +0530

    Roboshop scripts

--- shipping.sh
+++ shipping.sh
@@ -5,6 +5,7 @@ mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
 unzip /tmp/shipping.zip
+
 cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar

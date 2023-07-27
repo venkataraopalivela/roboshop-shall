ff93326b6fadc71bccd2275472bc06d88e0d3459
venkat <venkatapalivela.sap@gmail.com>
2023-07-27 17:40:24 +0530

    frontend update

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,27 @@
-yum install nginx -y
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+source common.sh
 
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo -e "\e[36m>>>>>>>>>> Install nginx >>>>>>>>>>>\e[0m"
+    yum install nginx -y
+    func_exit_status
 
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e[36m>>>>>>>>>> Copy RoboShop configuration >>>>>>>>>>>\e[0m"
+    cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+    func_exit_status
+
+echo -e "\e[36m>>>>>>>>>> Clear old content>>>>>>>>>>>\e[0m"
+   rm -rf /usr/share/nginx/html/*
+   func_exit_status
+
+echo -e "\e[36m>>>>>>>>>> Download Application content>>>>>>>>>>>\e[0m"
+   curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+   cd /usr/share/nginx/html
+   func_exit_status
+
+echo -e "\e[36m>>>>>>>>>> Extract Application content>>>>>>>>>>>\e[0m"
+   unzip /tmp/frontend.zip
+   func_exit_status
+
+echo -e "\e[36m>>>>>>>>>> Start Nginx service >>>>>>>>>>>\e[0m"
+   systemctl enable nginx
+   systemctl restart nginx
+   func_exit_status
